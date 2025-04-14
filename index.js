11b4633f09f7d0a7cf0c43ffc2c94e34c89245cb
gvdp <vdputteglenn@gmail.com>
2025-04-14 16:55:39 +0200

    feat: remove prettier config and peer dependency

--- index.js
+++ index.js
@@ -7,16 +7,8 @@ export default {
   name: '@bagaar/ember-template-lint-plugin',
   configurations: {
     recommended: {
-      extends: [
-        'a11y',
-        'bgr-rules:bgr-preferred',
-        'ember-template-lint-plugin-prettier:recommended',
-        'recommended',
-      ],
-      plugins: [
-        'ember-template-lint-plugin-prettier',
-        `${path.resolve(dirname, 'plugins/bgr.js')}`,
-      ],
+      extends: ['a11y', 'bgr-rules:bgr-preferred', 'recommended'],
+      plugins: [`${path.resolve(dirname, 'plugins/bgr.js')}`],
       rules: {
         'no-bare-strings': true,
         'require-each-key': true,
